c6e461e3f5bb10ba39a3748ed691d443e5c54421
열일 <isawthat77@naver.com>
2022-08-03 22:38:14 +0900

    투두 리스트 불러오기

--- client/src/todos/todoContainer.jsx
+++ client/src/todos/todoContainer.jsx
@@ -1,33 +1,67 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-
-
+import React, { useEffect, useState } from 'react'
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import TodoList from './todoList';
+import Todo from './todo';
+import './todos.css'
+const USER_TOKEN = JSON.parse(localStorage.getItem('token'))
 function TodoContainer() {
-    const token = JSON.parse(localStorage.getItem('token'))
+    const [todoList, setTodoList] = useState({})
     const navigate = useNavigate();
 
-    useEffect(()=>{
-        if(!token){
+    useEffect(() => {
+        if (!USER_TOKEN) {
             navigate('/login')
+        } else {
+            getTodoList()
         }
-    },[])
+    }, [])
 
-    const onClickLogout= () =>{
+    //todoList 불러오기
+    const getTodoList = async() => {
+        try {
+            const res = await axios.get('/todos',
+            {headers:{Authorization: USER_TOKEN}})
+            if (res.status === 200) {
+                setTodoList(res.data.data)
+            }
+          } catch (error) {
+            alert(error.response.data.details)
+          }
+    }
+
+    //로그아웃 이벤트
+    const onClickLogout = () => {
         localStorage.removeItem('token')
         navigate('/login')
     }
 
-  return (
-    <section className='wrapper'>
-        <header>
-            <button
-            onClick={onClickLogout}>로그아웃</button>
-        </header>
-        <div className='content'>
-            todos
-        </div>
-    </section>
-  )
+    return (
+        <section className='wrapper'>
+            <header>
+                <button
+                    onClick={onClickLogout}>로그아웃</button>
+            </header>
+            <section className='todo-body'>
+                <section className='todo-list'>
+                    <h1>Todo List</h1>
+                    <hr></hr>
+                    <TodoList USER_TOKEN={USER_TOKEN}
+                    getTodoList={getTodoList}
+                    todoList={todoList}
+                    ></TodoList>
+                </section>
+                <section className='todos'>
+                    <h1>Todo Info</h1>
+                    <hr></hr>
+                    <Routes>
+                        <Route exact={true} path='/' element={<>Todo List에서 상세 조회할 할일을 선택해주세요</>} />
+                        <Route exact={true} path=':num' element={<Todo />} />
+                    </Routes>
+                </section>
+            </section>
+        </section>
+    )
 }
 
 export default TodoContainer
--- client/src/todos/todoList.jsx
+++ client/src/todos/todoList.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
-function TodoList() {
+function TodoList({getTodoList, todoList, USER_TOKEN}) {
+  const [newTodo, setNewTodo] = useState({
+    title:'',
+    content:'',
+  })
+  const onChange = (e) =>{
+    setNewTodo({...newTodo, [e.target.id]:e.target.value})
+  }
+
+  const onSubmit = async() =>{
+    try {
+      const res = await axios.post('/todos',
+      newTodo,
+      {headers:{Authorization: USER_TOKEN}})
+      if (res.status === 200) {
+        getTodoList()
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  console.log(todoList)
   return (
-    <div>todoList</div>
+    <>
+      <section className='todo-content'>
+        {
+          todoList.map(item=>(
+            <div>
+            {item.title}
+            </div>
+          ))
+        }
+      </section>
+      <section className='todo-bottom'>
+        <form>
+          <input type='text'
+          id='title'
+          onChange={onChange}
+          placeholder='할일의 제목을 입력해주세요.'></input>
+          <input type='text'
+          id='content'
+          onChange={onChange}
+          placeholder='할일의 내용을 입력해주세요.'></input>
+        </form>
+        <button onClick={onSubmit}>등록하기</button>
+      </section>
+    </>
   )
 }
 
