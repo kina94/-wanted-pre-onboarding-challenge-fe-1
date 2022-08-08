35be62777bd7e3a0c56789ee3ffde53ebc1ddc1d
열일 <isawthat77@naver.com>
2022-08-09 01:44:35 +0900

    컴포넌트 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './views/Login';
 import SignUp from './views/SignUp';
-import TodoList from './views/TodoList';
+import Todo from './views/Todo';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path='/*' element={<TodoList/>}/>
+        <Route path='/*' element={<Todo/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/sign_up' element={<SignUp/>}/>
       </Routes>
---
+++ client/src/components/TodoInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function TodoInfo() {
+  return (
+    <div>TodoInfo</div>
+  )
+}
+
+export default TodoInfo
