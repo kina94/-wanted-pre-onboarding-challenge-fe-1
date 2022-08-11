ae806ddf61c0b35637575bb7317944ea588634ca
열일 <isawthat77@naver.com>
2022-08-11 21:53:40 +0900

    Refactor: todoService 함수명 뒤에 Api 추가

--- client/src/service/todoService.ts
+++ client/src/service/todoService.ts
@@ -4,19 +4,19 @@ import { doAxios } from "../utils/doAxios";
 const BASE_URL = "/todos";
 
 //투두 리스트 불러오기
-export const callGetTodos = async () => {
+export const callGetTodosApi = async () => {
   const response = await doAxios({ method: "get", url: BASE_URL });
   return response;
 };
 
 //아이디로 투두 상세 내용 조회
-export const callGetTodoById = async (id: string | undefined) => {
+export const callGetTodoByIdApi = async (id: string | undefined) => {
   const response = await doAxios({ method: "get", url: `${BASE_URL}/${id}` });
   return response;
 };
 
 //투두 생성
-export const callCreateTodo = async (data: TodoInput) => {
+export const callCreateTodoApi = async (data: TodoInput) => {
   const response = await doAxios({
     method: "post",
     url: `${BASE_URL}`,
@@ -26,7 +26,7 @@ export const callCreateTodo = async (data: TodoInput) => {
 };
 
 //투두 수정
-export const callUpdateTodo = async (id: string | undefined, data: Todo) => {
+export const callUpdateTodoApi = async (id: string | undefined, data: Todo) => {
   const response = await doAxios({
     method: "put",
     url: `${BASE_URL}/${id}`,
@@ -36,7 +36,7 @@ export const callUpdateTodo = async (id: string | undefined, data: Todo) => {
 };
 
 //투두 삭제
-export const callDeleteTodo = async (id: string) => {
+export const callDeleteTodoApi = async (id: string) => {
   const response = await doAxios({
     method: "delete",
     url: `${BASE_URL}/${id}`,
